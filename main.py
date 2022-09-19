f094020bb64bd8f4bbcf654aa9e95dbee387b91b
casiojapi <maximosantoro@protonmail.com>
2022-09-19 13:31:00 -0300

    functioning difficulty condition

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import hashlib
 import sys
 
 string_to_work = str(sys.argv[1])
-# diff = int(input("input the difficulty you want: "))
-max_iter = int(sys.argv[2])
+# max_iter = int(sys.argv[2])
 print("string: " + string_to_work)
-print("iterations: " + str(max_iter))
+# print("iterations: " + str(max_iter))
 print("start")
 
 min_diff = float('inf')
@@ -17,24 +16,21 @@ nonce = 0
 
 #TODO: la idea es hacerlo con un algoritmo de hashing mas hippie, que no escale ni con GPUs ni asics. RandomX? - para que sea mas o menos igual de costoso para cualquiera. No que puedan mandar el request a una nave de google y este en 1ms.
 
-for i in range(max_iter):
+while True:
     string_with_nonce = string_to_work + " " + str(nonce)
     hashed = hashlib.sha256(string_with_nonce.encode('utf-8')).hexdigest()
     # if i % 1000:
         # print("\t" + str((i * 100) / max_iter) + "%", end='\r')
-    
-    #TODO: no tener hardcodeada la diff, si no que dadas las max iteraciones, encontrar el hash con menor valor numerico.
 
     #TODO: Implementar una dificultad opcional dada por input, que si sale con esa, breakea el loop y ya te devuelve tu string con nonce y su hash.
-    if int(hashed, 16) < min_diff:
+    if int(hashed, 16) < int("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16):
         min_diff = int(hashed, 16)
         print(string_with_nonce)
         print(hashed)
         min_hash = hashed
         final_nonce = nonce
         best_hash = string_with_nonce
-
-        
+        break
 
     nonce += 1
 
